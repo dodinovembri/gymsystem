bf2a5646dd45024fb0f86f38dca1661eb3717723
dodinovembri <dodinovembri32gmail.com>
2020-10-16 16:44:22 +0700

    update sidebar

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -5,20 +5,38 @@
           <small class="sidebar-logo-headline">Gym Center System for Palembang and Surrounding</small>
         </div>
       </div><!-- sidebar-header -->
-      <div id="dpSidebarBody" class="sidebar-body">
-        <ul class="nav nav-sidebar">
-          <li class="nav-item"><a href="{{ route('dashboard') }}" class="nav-link {{ (Request::is('dashboard')) ? 'active' : '' }}"><i data-feather="box"></i> Dashboard</a></li>
-          <li class="nav-label"><label class="content-label">Transaction</label></li>
-          <li class="nav-item"><a href="{{ route('admin.alternative.index') }}" class="nav-link {{ (Request::is('admin/alternative/*')) ? 'active' : '' }}"><i data-feather="activity"></i> Alternative</a></li>
-          <li class="nav-item"><a href="{{ route('admin.ranking.index') }}" class="nav-link {{ (Request::is('admin/ranking/*')) ? 'active' : '' }}"><i data-feather="airplay"></i> Ranking List</a></li>          
-          <li class="nav-label"><label class="content-label">Master Data</label></li>
-          <li class="nav-item"><a href="{{ route('admin.criteria.index') }}" class="nav-link {{ (Request::is('admin/criteria/*')) || (Request::is('admin/criterion_value/*')) ? 'active' : '' }}"><i data-feather="layers"></i> Criteria</a></li>
-          <li class="nav-label"><label class="content-label">System Setting</label></li>
-          <li class="nav-item"><a href="{{ route('admin.user.index') }}" class="nav-link {{ (Request::is('admin/user/*'))  ? 'active' : '' }}"><i data-feather="user"></i> Users Account List</a></li>
-          <li class="nav-item"><a href="{{ route('admin.web_home.index') }}" class="nav-link  {{ (Request::is('admin/web_home/*'))  ? 'active' : '' }}"><i data-feather="menu"></i> Home</a></li>
-          <li class="nav-item"><a href="{{ route('admin.web_about_us.index') }}" class="nav-link  {{ (Request::is('admin/web_about_us/*'))  ? 'active' : '' }}"><i data-feather="menu"></i> About Us</a></li>
-          <li class="nav-item"><a href="{{ route('admin.news.index') }}" class="nav-link  {{ (Request::is('admin/news/*'))  ? 'active' : '' }}"><i data-feather="menu"></i> News</a></li>          
-        </ul>              
+      <?php 
+          $user_id = Auth()->user()->id;
+          $status = App\Models\UserAccountModel::find($user_id);
+          if ($status->role_id == 0) { ?>
+            <div id="dpSidebarBody" class="sidebar-body">
+              <ul class="nav nav-sidebar">
+                <li class="nav-item"><a href="{{ route('dashboard') }}" class="nav-link {{ (Request::is('dashboard')) ? 'active' : '' }}"><i data-feather="box"></i> Dashboard</a></li>
+                <li class="nav-label"><label class="content-label">Transaction</label></li>
+                <li class="nav-item"><a href="{{ route('admin.alternative.index') }}" class="nav-link {{ (Request::is('admin/alternative/*')) ? 'active' : '' }}"><i data-feather="activity"></i> Alternative</a></li>
+                <li class="nav-item"><a href="{{ route('admin.ranking.index') }}" class="nav-link {{ (Request::is('admin/ranking/*')) ? 'active' : '' }}"><i data-feather="airplay"></i> Ranking List</a></li>          
+                <li class="nav-label"><label class="content-label">Master Data</label></li>
+                <li class="nav-item"><a href="{{ route('admin.criteria.index') }}" class="nav-link {{ (Request::is('admin/criteria/*')) || (Request::is('admin/criterion_value/*')) ? 'active' : '' }}"><i data-feather="layers"></i> Criteria</a></li>
+                <li class="nav-label"><label class="content-label">System Setting</label></li>
+                <li class="nav-item"><a href="{{ route('admin.user.index') }}" class="nav-link {{ (Request::is('admin/user/*'))  ? 'active' : '' }}"><i data-feather="user"></i> Users Account List</a></li>
+                <li class="nav-item"><a href="{{ route('admin.web_home.index') }}" class="nav-link  {{ (Request::is('admin/web_home/*'))  ? 'active' : '' }}"><i data-feather="menu"></i> Home</a></li>
+                <li class="nav-item"><a href="{{ route('admin.web_about_us.index') }}" class="nav-link  {{ (Request::is('admin/web_about_us/*'))  ? 'active' : '' }}"><i data-feather="menu"></i> About Us</a></li>
+                <li class="nav-item"><a href="{{ route('admin.news.index') }}" class="nav-link  {{ (Request::is('admin/news/*'))  ? 'active' : '' }}"><i data-feather="menu"></i> News</a></li>          
+              </ul>              
 
-      </div><!-- sidebar-body -->
+            </div><!-- sidebar-body -->
+          <?php } else if($status->role_id == 1){ ?>
+            <div id="dpSidebarBody" class="sidebar-body">
+              <ul class="nav nav-sidebar">
+                <li class="nav-item"><a href="{{ route('dashboard') }}" class="nav-link {{ (Request::is('dashboard')) ? 'active' : '' }}"><i data-feather="box"></i> Dashboard</a></li>
+                <li class="nav-label"><label class="content-label">Transaction</label></li>
+                <li class="nav-item"><a href="{{ route('admin.alternative.index') }}" class="nav-link {{ (Request::is('admin/alternative/*')) ? 'active' : '' }}"><i data-feather="activity"></i> Alternative</a></li>
+                <li class="nav-item"><a href="{{ route('admin.ranking.index') }}" class="nav-link {{ (Request::is('admin/ranking/*')) ? 'active' : '' }}"><i data-feather="airplay"></i> Ranking List</a></li>          
+                <li class="nav-label"><label class="content-label">Master Data</label></li>
+                <li class="nav-item"><a href="{{ route('admin.criteria.index') }}" class="nav-link {{ (Request::is('admin/criteria/*')) || (Request::is('admin/criterion_value/*')) ? 'active' : '' }}"><i data-feather="layers"></i> Criteria</a></li>
+              </ul>              
+
+            </div><!-- sidebar-body -->
+          <?php }
+      ?>
     </div><!-- sidebar -->
